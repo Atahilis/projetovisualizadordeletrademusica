b697889dccfa6b82dd6b3b4bce67c8a8ffc0b08a
022310089 <022310089@uneb.br>
2024-05-22 11:55:42 -0300

    mensagem

---
+++ src/main/java/Model/Login.java
@@ -0,0 +1,29 @@
+package Model
+
+/main/java/Login;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Login {
+
+        @FXML
+        private Button login;
+
+        @FXML
+        private PasswordField senha;
+
+        @FXML
+        private TextField user;
+
+        @FXML
+        void loginAction(ActionEvent event) {
+
+        }
+
+    }
+
+
