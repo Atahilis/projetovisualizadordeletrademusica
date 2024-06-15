f9ba55850fb98001c64d4889fb8354ddefec4ed6
Atahilis <athyoliver@gmail.com>
2024-06-15 19:29:01 -0300

    news

---
+++ src/main/java/com/example/letrasdemusicas/Controller/TelaLetra.java
@@ -0,0 +1,25 @@
+package com.example.letrasdemusicas.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+
+public class TelaLetra {
+
+    @FXML
+    private ImageView capa;
+
+    @FXML
+    private TextArea letrasArea;
+
+    @FXML
+    private Hyperlink spotifyLink;
+
+    @FXML
+    void spotifyLinkAction(ActionEvent event) {
+
+    }
+
+}
