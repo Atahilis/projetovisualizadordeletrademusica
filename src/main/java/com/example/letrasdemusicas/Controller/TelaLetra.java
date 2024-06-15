056596b59f1f68de59c864b579829f17d16fa91f
Atahilis <athyoliver@gmail.com>
2024-06-15 20:52:37 -0300

    news

--- src/main/java/com/example/letrasdemusicas/Controller/TelaLetra.java
+++ src/main/java/com/example/letrasdemusicas/Controller/TelaLetra.java
@@ -1,12 +1,24 @@
 package com.example.letrasdemusicas.Controller;
 
+import com.example.letrasdemusicas.Model.Musica;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class TelaLetra {
+
+
+    @FXML
+    private Label MusicaArtista;
 
     @FXML
     private ImageView capa;
@@ -19,7 +31,30 @@ public class TelaLetra {
 
     @FXML
     void spotifyLinkAction(ActionEvent event) {
+        Desktop desktop = Desktop.getDesktop();
 
+        try {
+            desktop.browse(new URI(musica.getMusicalink()));
+        } catch (IOException | URISyntaxException e){
+            System.out.println("Erro ao abrir o link");
+        }
+
+    }
+    private Musica musica;
+
+    public void iniciar(Musica musica){
+        this.musica = musica;
+
+        String nomeMusicaArtista = musica.getTitulo() + "\n" + musica.getArtista();
+
+        MusicaArtista.setWrapText(true);
+        MusicaArtista.setText(nomeMusicaArtista);
+
+        letrasArea.setText(this.musica.getCapaLink());
+
+        Image imagemCapa = new Image(this.musica.getCapaLink());
+
+        capa.setImage(imagemCapa);
     }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.letrasdemusicas {
     requires org.apache.httpcomponents.client5.httpclient5;
     requires org.apache.httpcomponents.core5.httpcore5;
     requires org.json;
+    requires java.desktop;
 
 
     opens com.example.letrasdemusicas.Model to javafx.fxml;
